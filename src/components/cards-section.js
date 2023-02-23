d68f9ab1258f3e9d03e3bf0bbd8b64df186ab2b8
elizabeth lands <elizabeth_lands@hotmail.com>
2023-02-23 14:54:14 +0000

    feat: add responsive cards layout and adjust header size

--- src/components/cards-section.js
+++ src/components/cards-section.js
@@ -1,12 +1,19 @@
 import styled from 'styled-components';
 
-const StyledCard = styled.div`
+const StyledCards = styled.div`
   border: 1px solid var(--black);
-  margin-top: 50px;
+  margin: 0 auto 50px;
+  display: grid;
+  gap: 20px;
+  grid-template-columns: repeat( auto-fit, minmax(250px, 1fr) );
+  img {
+    margin: 0 auto;
+  } 
 `;
 
 const Card = styled.div`
   margin: 20px auto;
+  border: 1px solid green;
 `;
 
 const Text = styled.div`
@@ -15,13 +22,13 @@ const Text = styled.div`
 
 export default function CardsSection({ sortedData }) {
   return (
-    <StyledCard>
+    <StyledCards>
       {sortedData?.map(item => (
         <Card key={item.node.id}>
           <Text>{item.node.name}</Text>
           <Text>{item.node.rating}</Text>
         </Card>
       ))}
-    </StyledCard>
+    </StyledCards>
   )
 }
--- src/components/header.js
+++ src/components/header.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 const Heading = styled.div`
   text-align: center;
   font-size: 2rem;
+  height: 100px;
 `;
 
 const Header = () => {
