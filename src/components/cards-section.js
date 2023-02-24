01b320629a8801b23386dbc1fe783be00a448bc4
elizabeth lands <elizabeth_lands@hotmail.com>
2023-02-24 09:34:49 +0000

    feat: setup overlay window

--- src/components/cards-section.js
+++ src/components/cards-section.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import styled from 'styled-components';
 
 const Cards = styled.div`
@@ -58,38 +59,90 @@ const Unit = styled.div`
   text-transform: lowercase;
 `;
 
-function chooseFeatured() {
-  console.log("featured")
-}
+const StyledFeaturedCard = styled.div`
+  position: fixed;
+  top: 40%;
+  left: 50%;
+  width: 30em;
+  height: 36em;
+  margin-top: -18em;
+  margin-left: -18em;
+  z-index: 99;
+`;
+
+const StyledOverlay = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  background: black;
+  opacity: 0.8;
+  height: 100%;
+  width: 100%;
+  z-index: 90;
+`;
+
+const CloseButton = styled.button`
+  padding: 8px;
+  font-size: 1.1rem;
+  width: 40px;
+`;
 
 export default function CardsSection({ sortedData }) {
+
+  const [featuredCard, setFeaturedCard] = useState();
+
+  const [viewDetails, setViewDetails] = useState({
+    id: 0,
+    isOpened: false,
+  });
+
+  const selectCard = (id) => {
+    if (!viewDetails.isOpened) {
+      setViewDetails({
+        id: id,
+        isOpened: true,
+      });
+    }
+    let selection = sortedData.find(item => item.node.id === id);
+    selection && setFeaturedCard(selection);
+    console.log(featuredCard)
+  };
+
   return (
-    <Cards>
-      {sortedData?.map(item => (
+    <>
+      {viewDetails.isOpened && 
+        <>
+          <StyledOverlay/>
+          <StyledFeaturedCard>
+            <CloseButton onClick={() => setViewDetails({id: 0, isOpened: false})}>
+              X
+            </CloseButton>
 
-        <Card key={item.node.id} onClick={() => chooseFeatured()}>
+            <div>Test</div>
+          </StyledFeaturedCard>
+        </>
+      }
 
-          <img src={item.node.thumbnail?.url} alt={item.node.thumbnail?.alt}></img> 
-         
-          <Name>{item.node.name}</Name>
-
-          <Category>{item.node.category?.name}</Category>
-
-          <Description>{item.node?.seoDescription}</Description>
-
-          <PriceWeightWrapper>
-
-            <div>£{item.node.pricing.priceRange.stop.gross?.amount.toFixed(2)}</div>
-
-            <WeightWrapper>
-              <Weight>{item.node.weight?.value.toFixed(2)}</Weight>
-              <Unit>{item.node.weight?.unit}</Unit>
-            </WeightWrapper>
-
-          </PriceWeightWrapper>
-         
-        </Card>
-      ))}
-    </Cards>
+      <Cards>
+        {sortedData?.map(item => (
+          <Card key={item.node.id} onClick={() => selectCard(item.node.id)}>
+            <img src={item.node.thumbnail?.url} alt={item.node.thumbnail?.alt}></img>
+            <Name>{item.node.name}</Name>
+            <Category>{item.node.category?.name}</Category>
+            <Description>{item.node?.seoDescription}</Description>
+            <PriceWeightWrapper>
+              <div>£{item.node.pricing.priceRange.stop.gross?.amount.toFixed(2)}</div>
+              <WeightWrapper>
+                <Weight>{item.node.weight?.value.toFixed(2)}</Weight>
+                <Unit>{item.node.weight?.unit}</Unit>
+              </WeightWrapper>
+            </PriceWeightWrapper>
+          </Card>
+        ))}
+      </Cards>
+    </>
   )
 }
+
+         
+     
